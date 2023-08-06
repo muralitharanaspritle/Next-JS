bf3e06a47737d652cd6e5069bffa3ee74cdff53c
Muralitharan <97423613+muralitharanaspritle@users.noreply.github.com>
2023-08-06 22:42:59 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          SPRITLE
+          SPRITLEs
           <code className={styles.code}>src/app/page.js</code>
         </p>
         <div>
